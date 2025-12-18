6b33e3edef410204f8635ed73fe516857399d9d7
Prakashram J <113188901+ramram128@users.noreply.github.com>
2025-12-18 18:45:56 +0530

    Add files via upload

---
+++ script01.js
@@ -0,0 +1,24 @@
+const greeting = document.getElementById("greeting");
+const hour = new Date().getHours();
+
+if (hour < 12) {
+    greeting.textContent = "Good Morning ";
+} else if (hour < 18) {
+    greeting.textContent = "Good Afternoon ";
+} else {
+    greeting.textContent = "Good Evening ";
+}
+
+const toggleBtn = document.getElementById("toggleBtn");
+const aboutText = document.getElementById("aboutText");
+
+
+toggleBtn.addEventListener("click", () => {
+    if (aboutText.style.display === "none") {
+        aboutText.style.display = "block";
+        toggleBtn.textContent = "Hide Bio";
+    } else {
+        aboutText.style.display = "none";
+        toggleBtn.textContent = "Show Bio";
+    }
+});
